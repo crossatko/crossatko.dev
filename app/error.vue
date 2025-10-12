94d7e9b6e5a7f0dd724efb8757ce8c5481285b48
Paul Cross <me@paulcross.cz>
2025-10-12 19:03:19 +0200

    fix error patge

--- app/error.vue
+++ app/error.vue
@@ -11,13 +11,13 @@ const handleError = () => clearError({ redirect: '/' })
 
 <template>
   <div class="flex flex-col items-center justify-start py-[4vw]">
-    <ContentProseH1>{{ error?.statusCode }}</ContentProseH1>
+    <ContentProseH1 fontSize="22vw">{{ error?.statusCode }}</ContentProseH1>
 
     <button
       @click="handleError"
       class="font-headline inline-flex bg-zinc-300 px-8 py-6 text-black duration-200 focus-within:ring-4 focus-within:ring-zinc-400 hover:bg-white"
     >
-      Clear errors
+      Go back home
     </button>
   </div>
 </template>
